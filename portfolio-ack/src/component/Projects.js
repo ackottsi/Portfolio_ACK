e9746537e101f8cfe028da673b19b10b70a827c1
ackottsi <kottsickandrewc@johndeere.com>
2021-03-19 23:23:20 -0500

    stopping point.  Need to better organize Projects tab and clean up Work Experience Page

--- portfolio-ack/src/component/Projects.js
+++ portfolio-ack/src/component/Projects.js
@@ -1,17 +1,45 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import '../Projects.css';
+
 
 
 
 const Projects = (props) => {
     return (  
   <div className="projects-container">
+
       <div className="project-info-container">
           <h2>Project 1-Simon Says</h2>
           <p>
-              This is the first project I developed using vanilla javascript.
-
+              This is the first project I developed independently using vanilla javascript.  &nbsp;  
           </p>
+          <Link className="deployed-link" to='add-site'>Deployed Site</Link>
+          <Link className="deployed-link" to='add-site'>Git Hub repo</Link>
+      </div>
+
+
+      <div className="project-info-container">
+          <h2>Project 2-Express</h2>
+          <p>
+              This is the first project where I used Express.  &nbsp;  Admittedly, there were quite a few 
+              struggles on this project.  I have left it at the current state as a reminder of the progress
+              I have made over a short amount of time.
+          </p>
+          <Link className="deployed-link" to='add-site'>Deployed Express App</Link>
+          <Link className="deployed-link" to='add-site'>Git Hub repo</Link>
+      </div>
+     
+
+
+      <div className="project-info-container">
+          <h2>Project 3-React app</h2>
+          <p>
+              This was a group project. &nbsp; React was the emphasis of the project.  &nbsp;
+                
+          </p>
+          <Link className="deployed-link" to='add-site'>React app</Link>
+          <Link className="deployed-link" to='add-site'>Git Hub repo</Link>
       </div>
      
     </div>
