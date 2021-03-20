59ecc92992e146d46b3c4ae49978aa8c6e024697
ackottsi <kottsickandrewc@johndeere.com>
2021-03-19 20:55:00 -0500

    changed app to class

--- portfolio-ack/src/App.js
+++ portfolio-ack/src/App.js
@@ -1,12 +1,28 @@
 
 import './App.css';
+import {Route, Switch, withRouter} from 'react-router-dom';
+import Header from './component/Header'
+import HomePage from './component/HomePage'
+import { Component } from 'react';
 
-function App() {
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={};
+  }
+  
+
+
+
+
+  rendera(){
   return (
     <div className="App">
       testing page
     </div>
   );
+}
 }
 
 export default App;
