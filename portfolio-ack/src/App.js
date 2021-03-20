0ea94bca29a8ca4d32e862c4cd05da09cb7f428f
ackottsi <kottsickandrewc@johndeere.com>
2021-03-19 21:02:31 -0500

    completed install for react router dom

--- portfolio-ack/src/App.js
+++ portfolio-ack/src/App.js
@@ -11,18 +11,39 @@ class App extends Component{
     super(props);
     this.state={};
   }
+
+
+
+
+
+  render(){
+    
+    return (
+
   
-
-
-
-
-  rendera(){
-  return (
-    <div className="App">
-      testing page
-    </div>
-  );
-}
-}
-
-export default App;
+            <div className="App">
+  
+              <Header handleChange={this.handleChange} userLogin={this.userLogin}
+                    username={this.state.username} password={this.state.password} userId={this.state.userId} />
+                    
+            <div className="main-body">
+              <Switch>
+              
+              <Route exact path="/" render={(routerProps)=>(
+                    <HomePage username={this.state.username} loggedIn={this.state.loggedIn}
+                    logout={this.logout} {...routerProps}/>
+                )}/>
+  
+                <Route exact path="/Projects" render={(routerProps)=>(
+                    <Signup handleChangeSignUp={this.handleChangeSignUp} userSignup={this.userSignup}
+                    usernameSignUp={this.state.usernameSignUp} passwordSignUp={this.state.passwordSignUp} userId={this.state.userId} {...routerProps}/>
+                )}/>
+  
+    
+                </Switch>
+               </div>   
+      </div>
+        );
+      }
+    }
+  export default withRouter(App);
