65eefb4364c8bb9e890a850285cf80539a4897fe
ackottsi <kottsickandrewc@johndeere.com>
2021-04-01 13:01:52 -0500

    aligned links to bottom of card

--- portfolio-ack/src/component/Projects.js
+++ portfolio-ack/src/component/Projects.js
@@ -21,9 +21,12 @@ const Projects = (props) => {
             </CardBody>
             <CardBody>
               <CardText>This is the first project I developed independently using vanilla javascript.</CardText>
-              <CardLink href="#">github link</CardLink>
-              <CardLink href="#">deployed app</CardLink>
-            </CardBody>
+              </CardBody>
+             <div className='link-container'>
+              <CardLink href="https://github.com/ackottsi/GA-Project-One">github repo</CardLink>
+              <CardLink href="https://ackottsi.github.io/">deployed app</CardLink>
+              </div>
+            
           </Card>
         
 
@@ -38,9 +41,12 @@ const Projects = (props) => {
               <CardText>First server side project and first group project.  Primary goals of project
                   were to learn about production deployment and using git for collaboration.
               </CardText>
-              <CardLink href="#">Card Link</CardLink>
-              <CardLink href="#">Another Link</CardLink>
-            </CardBody>
+              </CardBody>
+              <div className='link-container'>
+              <CardLink href="https://github.com/ackottsi/Project_2">github repo</CardLink>
+              <CardLink href="https://movie-app-ack.herokuapp.com/">deployed app</CardLink>
+              </div>
+           
           </Card>
 
 
@@ -53,9 +59,13 @@ const Projects = (props) => {
             </CardBody>
             <CardBody>
               <CardText>This is a group project that is built using React.  Uses omdb API to provide data.</CardText>
-              <CardLink href="#">Card Link</CardLink>
-              <CardLink href="#">Another Link</CardLink>
-            </CardBody>
+            </CardBody>             
+        
+              <div className='link-container'>
+              <CardLink href="#">github repo</CardLink>
+              <CardLink href="#">deployed app</CardLink>
+              </div>
+          
           </Card>
 
 
@@ -67,11 +77,14 @@ const Projects = (props) => {
               <CardSubtitle tag="h6" className="mb-2 text-muted">Photo Gallery App</CardSubtitle> 
             <img width="80%" src="https://imagebucketgaackproject.s3.us-east-2.amazonaws.com/Imagefolder/Project+4+screen+shot.png" alt="Card image cap" />
             </CardBody>
-            <CardBody>
+            <CardBody className="bottom-card-container">
               <CardText>First full stack project.  Server side is deployed through Heroku.</CardText>
-              <CardLink href="#">Git repo</CardLink>
-              <CardLink href="#">Deployed App</CardLink>
-            </CardBody>
+              </CardBody>
+              <div className='link-container'>
+              <CardLink href="#">github repo</CardLink>
+              <CardLink href="#">deployed app</CardLink>
+              </div>
+         
           </Card>
      
         
